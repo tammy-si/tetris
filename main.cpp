fc663dd6d3d2eae923a4a4a034297408a899719e
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-01-17 21:39:28 -0800

    Draws out the first block

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <random>
+#include <time.h>
 using namespace sf;
 // clang++ main.cpp -I/opt/homebrew/Cellar/sfml/2.5.1_2/include/ -L/opt/homebrew/Cellar/sfml/2.5.1_2/lib  -lsfml-graphics -lsfml-window -lsfml-system -std=c++20
 
@@ -14,26 +15,35 @@ struct Point {
     int y;
 };
 
+Point curr_block[4];
+
 // to make the pieces. Make the blocks based of the change from the main_point.
 int tetromino[7][4][2] = {
-   {{0,1}, {0,0}, {1,0}, {2,0}},    // J block
-   {{-1,0}, {0,0}, {0,1}, {1,0}},   // T block
-   {{-1,1}, {0,0}, {0,1}, {1,0}},   // Z block
-   {{-1,0}, {0,0}, {0,1}, {1,1}},   // S block
-   {{0,1}, {0,0}, {1,1}, {1,0}},    // O block
-   {{-1,0}, {0,0}, {1,0}, {2,0}},   // I block
-   {{-1,0}, {0,0}, {1,0}, {1,1}}    // L block
+   {{0,-1}, {0,0}, {1,0}, {2,0}},    // J block
+   {{-1,0}, {0,0}, {0,-1}, {1,0}},   // T block
+   {{-1,-1}, {0,0}, {0,-1}, {1,0}},   // Z block
+   {{-1,0}, {0,0}, {0,-1}, {1,-1}},   // S block
+   {{0,-1}, {0,0}, {1,-1}, {1,0}},    // O block
+   {{-1,-1}, {0,-1}, {1,-1}, {2,-1}},   // I block
+   {{-1,0}, {0,0}, {1,0}, {1,-1}}    // L block
 };
-
-
 
 int main()
 {
+    srand(time(NULL));
     RenderWindow window(VideoMode(COLUMNS * CELL_SIZE * RESIZE, ROWS * CELL_SIZE * RESIZE), "Tetris!");
+    // uploading original texture 
+    Texture texture;
+    texture.loadFromFile("Blocks.png");
+    // keep track of what block we're on
+    int block_num;
+    window.setFramerateLimit(60);
     int field[ROWS][COLUMNS];
-    Point main_point;
-    main_point.x = 4;
-    main_point.y = 1;
+    // making the first starter_point
+    Point starter_point;
+    starter_point.x = 4;
+    starter_point.y = 1;
+    bool made_block = false;
     while (window.isOpen())
     {
         Event event;
@@ -54,14 +64,27 @@ int main()
                 window.draw(block);
             }
         }
-        Texture texture;
-        texture.loadFromFile("Blocks.png");
-        Sprite sprite;
-        sprite.setTexture(texture);
-        sprite.setTextureRect(sf::IntRect(0, 0, 18, 18));
-        sprite.setScale(RESIZE, RESIZE);
-        sprite.setPosition(main_point.x * RESIZE * CELL_SIZE, main_point.y * RESIZE * CELL_SIZE);
-        window.draw(sprite);
+        // populate curr_block based on random block chosen
+        if (!made_block) {
+            block_num = rand() % 7;
+            for (int i = 0; i < 4; i++) {
+                curr_block[i].x = starter_point.x + tetromino[block_num][i][0];
+                curr_block[i].y = starter_point.y + tetromino[block_num][i][1];
+                std::cout << curr_block[i].x << " " << curr_block[i].y << std::endl;
+            }
+            std::cout << block_num << std::endl;
+            made_block = true;
+        }
+
+        // draw out curr block
+        for (int i = 0; i < 4; i++) {
+            Sprite sprite;
+            sprite.setTexture(texture);
+            sprite.setTextureRect(sf::IntRect(18 * block_num, 0, 18, 18));
+            sprite.setScale(RESIZE, RESIZE);
+            sprite.setPosition(curr_block[i].x * RESIZE * CELL_SIZE, curr_block[i].y * RESIZE * CELL_SIZE);
+            window.draw(sprite);
+        }            
 
     }
     return 0;
